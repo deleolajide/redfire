20c184141cefd554f4a324d88bcaee2edcd227de
olajide.dele <olajide.dele@a7e1b3f6-502d-11de-ac5b-8d3973fc8d9f>
2011-04-30 10:51:01 +0000

    Refractoring 2

--- trunk/src/client/redspark/c.cmd
+++ trunk/src/client/redspark/c.cmd
@@ -1,6 +1,6 @@
 del redspark.swf
 
-call C:\Projects\flex33\bin\mxmlc.exe  -locale= redspark.mxml
+call C:\Projects\flex45\bin\mxmlc.exe  -locale= redspark.mxml
 
 copy redspark.swf "C:\Program Files\Openfire\plugins\redfire\redspark\redspark.swf"
 copy redspark.swf "..\..\..\plugin\redspark\redspark.swf"
