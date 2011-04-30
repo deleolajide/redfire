785a62be0a93b6c6a408530dc71925673ab10d2c
olajide.dele <olajide.dele@a7e1b3f6-502d-11de-ac5b-8d3973fc8d9f>
2011-04-30 10:39:57 +0000

    Refractoring part 1

--- trunk/build/build370.cmd
+++
@@ -1,9 +0,0 @@
-call ant war >build.txt
-
-rd "D:\Program Files\Openfire\plugins\redfire" /q /s
-del "D:\Program Files\Openfire\plugins\redfire.war"
-copy "D:\Work\Projects\2010.04.21-iTrader\Workspace\openfire_3_6_4\target\openfire\plugins\redfire.war" "D:\Program Files\Openfire\plugins"
-
-del "D:\Program Files\Openfire\logs\*.*"
-
-pause
